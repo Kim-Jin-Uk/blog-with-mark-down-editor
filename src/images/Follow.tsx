13c203dfb4efdec36d91f1e1cc0fe11dd3ff1b34
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2023-06-03 17:54:41 +0900

    FIX: svg가 text보다 heigh가 커서 버튼이 커지는 오류 수정

--- src/images/Follow.tsx
+++ src/images/Follow.tsx
@@ -7,6 +7,7 @@ const Follow = () => {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className="svg--inner-button--height-24px"
     >
       <path
         fillRule="evenodd"
