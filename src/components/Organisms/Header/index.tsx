2d2a92398b5590a1949ae3e5dbda79f0631a7f4f
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2023-06-20 21:01:04 +0900

    UPDATE: 헤더 컴포넌트에 네비게이션, 버튼 컴포넌트 적용

--- src/components/Organisms/Header/index.tsx
+++ src/components/Organisms/Header/index.tsx
@@ -1,17 +1,47 @@
 import React, { useState } from "react";
-import Icon from "@/components/Atoms/SVG/Icon";
+import Icon from "@/components/Atoms/SVG/Logo";
 import { navMenus } from "./constants";
+import Nav from "@/components/Atoms/Nav";
+import Button from "@/components/Atoms/Button";
+import { convertObjectToButtonInterface } from "@/components/Atoms/Button/utils";
 
 const Header = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <header>
       <Icon></Icon>
-      <nav>
-        {navMenus.map((menu) => {
-          return <div>{menu}</div>;
-        })}
-      </nav>
+      <Nav items={navMenus}></Nav>
+      <div style={{ flex: 1 }}></div>
+      {isLogin ? (
+        <div></div>
+      ) : (
+        <>
+          <Button
+            buttonInterface={convertObjectToButtonInterface({
+              shape: "round",
+              background: "white",
+              color: "black",
+              hasBorder: false,
+              height: 36,
+              fontSize: 14,
+            })}
+          >
+            로그인
+          </Button>
+          <Button
+            buttonInterface={convertObjectToButtonInterface({
+              shape: "round",
+              background: "blue",
+              color: "white",
+              hasBorder: false,
+              height: 36,
+              fontSize: 14,
+            })}
+          >
+            회원가입
+          </Button>
+        </>
+      )}
     </header>
   );
 };
