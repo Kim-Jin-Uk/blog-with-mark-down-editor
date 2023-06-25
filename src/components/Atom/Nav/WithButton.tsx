bc1ebd3ea85ebe1e277add7aaf26d2ced42cdbcf
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2023-06-25 18:55:43 +0900

    FEATURE: 네비게이션 컴포넌트 추가
    메인페이지에서 사용될 버튼 그룹 형태의 네비게이션 컴포넌트

---
+++ src/components/Atom/Nav/WithButton.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Button from "../Button";
+import { convertObjectToButtonInterface } from "../Button/utils";
+
+const NavWithButton = ({
+  itemsMap,
+  selectedIndex,
+  setSelectedIndex,
+}: {
+  itemsMap: Map<string, string>;
+  selectedIndex: number;
+  setSelectedIndex: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+  return (
+    <nav>
+      <ul>
+        {Array.from(itemsMap.keys()).map((item, idx) => (
+          <li style={{ padding: 4 }}>
+            <Button
+              key={item}
+              buttonInterface={convertObjectToButtonInterface({
+                shape: "round",
+                background: "blue",
+                color: "white",
+                hasBorder: false,
+                height: 32,
+                width: 172,
+                fontSize: 16,
+                align: "left",
+                isNotActive: idx !== selectedIndex,
+              })}
+              svg={itemsMap.get(item)}
+              onClick={() => setSelectedIndex(idx)}
+            >
+              {item}
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavWithButton;
