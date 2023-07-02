f1a15a4b8ccb59c7c36187ee2b059dce46a2d1c4
김진욱 <76549646+Kim-Jin-Uk@users.noreply.github.com>
2023-07-02 11:19:06 +0900

    FEATURE: 동일한 타입의 여러 ref, state 설정을 돕는 훅 생성

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,4 +1,11 @@
 import { BasicObject } from "@/common/types";
+import {
+  Dispatch,
+  MutableRefObject,
+  SetStateAction,
+  useRef,
+  useState,
+} from "react";
 
 /**
  * 객체 형태의 class names를 문자열로 변환해주는 함수
@@ -20,4 +27,30 @@ export const classNames = (classes: BasicObject<boolean>) => {
  */
 export const convertCamelToKebabCase = (camelCaseStr: string) => {
   return camelCaseStr.replace(/[A-Z]/g, (letter) => `-${letter.toLowerCase()}`);
+};
+/**
+ * 초기값 배열을 입력받아 useRef 배열을 반환하는 함수
+ *
+ * @param values ref 훅을 설정할 초기값 배열
+ * @returns useRef 배열
+ */
+export const useRefs = <T>(values: T[]) => {
+  const refs: MutableRefObject<T>[] = [];
+  for (const value of values) {
+    refs.push(useRef<T>(value));
+  }
+  return refs;
+};
+/**
+ * 초기값 배열을 입력받아 useState 배열을 반환하는 함수
+ *
+ * @param values state 훅을 적용할 초기값 배열
+ * @returns useState 배열
+ */
+export const useStates = <T>(values: T[]) => {
+  const states: [T, Dispatch<SetStateAction<T>>][] = [];
+  for (const value of values) {
+    states.push(useState<T>(value));
+  }
+  return states;
 };
